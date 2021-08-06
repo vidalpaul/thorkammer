4629f4c5877b343ab8f163a4754a1a41c5cf3b3e
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-06 21:24:05 +0100

    work on landing page

---
+++ pages/records/[record].tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecordDetail = () => {
+  return <div></div>;
+};
+
+export default RecordDetail;
---
+++ pages/records/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecordsPage = () => {
+  return <div></div>;
+};
+
+export default RecordsPage;
